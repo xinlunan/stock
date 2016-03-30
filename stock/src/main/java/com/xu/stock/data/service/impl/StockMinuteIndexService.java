b15c356fef6af94cd8e1537662da674a092de490
xulunan <xulunan@7827d465-bf64-4f94-afec-c3a403b591a3>
2016-03-30 11:57:24 +0000

    股票分时指数

---
+++ stock/src/main/java/com/xu/stock/data/service/impl/StockMinuteIndexService.java
@@ -0,0 +1,44 @@
+package com.xu.stock.data.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.xu.stock.data.dao.IStockMinuteIndexDao;
+import com.xu.stock.data.model.StockMinuteIndex;
+import com.xu.stock.data.service.IStockMinuteIndexService;
+
+/**
+ * 股票指数service实现
+ * 
+ * @version Revision History
+ * 
+ *          <pre>
+ * Author     Version       Date        Changes
+ * lunan.xu    1.0           2015-5-23     Created
+ * 
+ *          </pre>
+ * 
+ * @since 1.
+ */
+@SuppressWarnings("restriction")
+@Service("stockMinuteIndexService")
+public class StockMinuteIndexService implements IStockMinuteIndexService {
+	static Logger log = LoggerFactory.getLogger(StockMinuteIndexService.class);
+
+	@Resource
+	private IStockMinuteIndexDao stockMinuteIndexDao;
+
+	public List<StockMinuteIndex> getStockMinuteIndexs(String stockCode, Date date) {
+		return stockMinuteIndexDao.getStockMinuteIndexs(stockCode, date);
+	}
+
+	public void saveStockMinuteIndexs(List<StockMinuteIndex> stockMinuteIndexs) {
+		stockMinuteIndexDao.saveStockMinuteIndexs(stockMinuteIndexs);
+	}
+}
